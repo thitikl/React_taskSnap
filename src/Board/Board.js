64661f25a704f2b9419bbd8d48b983be262e1412
tony-kl <n01536671@humber.ca>
2023-08-06 22:41:33 -0400

    Edit sample text messge on Board page

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,16 +1,6 @@
 export default function Board(props){
-    const renderTasks = () => {
-        return props.data.slice(5,10).map((task) => {
-            return <div>
-                <h3>{task.title}</h3>
-                <p>{task.assigned_to}</p>
-                <p>{task.due}</p>
-            </div>
-        })
-    };
             
     return<div className="content">
-        <h1>Board</h1>
-        {renderTasks()}
+        <h1>Board Content will be here in the final product</h1>
     </div>
 }
