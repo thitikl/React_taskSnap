584a991d56212c2d788d29a6075cb0d9ea682baf
Himanshu Jain <n01210458@humbermail.ca>
2023-08-09 19:38:36 -0400

    CSS updates and description added

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -45,23 +45,27 @@ export default function Board(props) {
             <p>
               <span style={{ color: "#f9eaea", fontSize: "18px" }}>
                 <strong>{task.title}</strong>
+                
+              </span>
+              {task.description && (
+                <>
                 <br />
-              </span>
-              <span style={{ color: "silver", fontSize: "14px" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-tags-fill"
-                  viewBox="0 0 16 16"
+                <span
+                  style={{
+                    color: "#ededed",
+                    fontSize: "12px",
+                    msOverflowStyle: "none",
+                    scrollbarWidth: "none",
+                    margin: "none",
+                    padding: "none",
+                  }}
                 >
-                  <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                  <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
-                </svg>{" "}
-                {task.label}
-              </span>
+                  {task.description}
+                </span>
+                </>
+              )}
               <br />
+
               <span style={{ color: "#ededed", fontSize: "14px" }}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -77,6 +81,20 @@ export default function Board(props) {
                 {formatDate(task.due)}
               </span>
               <br />
+              <span style={{ color: "silver", fontSize: "14px" }}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-tags-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                  <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
+                </svg>{" "}
+                {task.label}
+              </span>
               <span style={{ color: "#ededed", fontSize: "14px" }}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
