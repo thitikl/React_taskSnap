1ed29454410deb3b82915e56e4705921a3c5b046
tony-kl <n01536671@humber.ca>
2023-08-13 16:30:09 -0400

    Edit logic on Calendar
    Use Hook for tasks

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -10,6 +10,8 @@ const localizer = momentLocalizer(moment)
 export default function TaskSnapCalendar(props) {
     const [holidays, setHoliday] = useState([]);
     const HOLIDAY_API_URL = 'https://canada-holidays.ca/api/v1/holidays';
+
+    // Fetching holiday data from API
     useEffect(() => {
         fetch(HOLIDAY_API_URL)
             .then(res => res.json())
@@ -29,17 +31,22 @@ export default function TaskSnapCalendar(props) {
         }
     });
 
-    const tasks = [];
-    props.data.forEach(task => {
-        tasks.push({
+    // Fetching event data from props
+    const [tasks, setTasks] = useState([]);
+    function taskToEvent(task) {
+        return {
             title: task.title,
             start: moment(task.start).toDate(),
             end: moment(task.due).toDate(),
             allDay: task.due.includes('T') ? false : true,
-        })
-    });
+        }
+    }
+    const newTasks = props.data.map(taskToEvent);
+    useEffect(() => { setTasks(newTasks) }, []);
 
+    // Combine data from API and props
     const events = [...holidayEvents, ...tasks];
+
     return <div className='content'>
         <Calendar
             className='main-calendar'
