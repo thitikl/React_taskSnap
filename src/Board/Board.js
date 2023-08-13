cf537ecb6910718d10fdab0e91879a4a53e35582
Himanshu <n01210458@humbermail.ca>
2023-08-13 01:42:42 -0400

    CSS Improved and D&D Added

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,278 +1,186 @@
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  finishedMain,
+  finishedSub,
+  upcomingMain,
+  upcomingSub,
+  ongoingMain,
+  ongoingSub,
+  headingcss,
+  taskDescription,
+} from "./BoardStyles";
+import { formatDate } from "./DateFormat";
+import { resetServerContext } from "react-beautiful-dnd";
 
 export default function Board(props) {
-  function formatDate(dateString) {
-    const date = new Date(dateString);
-    if (isNaN(date)) {
-      return ""; // Invalid date format
-    }
-
-    const options = { year: "numeric", month: "short", day: "2-digit" };
-    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
-      date
-    );
-
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let ampm = hours >= 12 ? "pm" : "am";
-
-    // Convert hours to 12-hour format
-    if (hours > 12) {
-      hours -= 12;
-    } else if (hours === 0) {
-      hours = 12; // Midnight
-    }
-
-    const formattedTime =
-      hours !== 0 || minutes !== 0
-        ? `${hours}:${minutes.toString().padStart(2, "0")}${ampm}`
-        : "11:59pm";
-
-    return `${formattedDate} [${formattedTime}]`;
-  }
-
   const { data: tasksData } = props;
-  const renderTasks = (tasks) => {
-    return tasks.length > 0 ? (
-      tasks.map((task) => (
-        <div
-          className="tasks"
-          style={{ cursor: "pointer" }}
-          key={task.id}
-          onClick={() => alert("Task Edit Pop Will Be here")}
-        >
-          <div className="container">
-            <p>
-              <span style={{ color: "#f9eaea", fontSize: "18px" }}>
-                <strong>{task.title}</strong>
-                
-              </span>
-              {task.description && (
-                <>
-                <br />
-                <span
-                  style={{
-                    color: "#ededed",
-                    fontSize: "12px",
-                    msOverflowStyle: "none",
-                    scrollbarWidth: "none",
-                    margin: "none",
-                    padding: "none",
-                  }}
-                >
-                  {task.description}
-                </span>
-                </>
-              )}
-              <br />
+  const [tasks, setTasks] = useState(tasksData);
 
-              <span style={{ color: "#ededed", fontSize: "14px" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-calendar-event-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
-                </svg>
-                <span> </span>
-                {formatDate(task.due)}
-              </span>
-              <br />
-              <span style={{ color: "silver", fontSize: "14px" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-tags-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                  <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
-                </svg>{" "}
-                {task.label}
-              </span>
-              <span style={{ color: "#ededed", fontSize: "14px" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-chevron-double-right"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"
-                  />
-                </svg>{" "}
-                {task.assigned_to}
-              </span>
-            </p>
-          </div>
-        </div>
-      ))
-    ) : (
-      <p>No tasks for this status</p>
-    );
+  const handleOnDragEnd = (result) => {
+    try {
+      const { source, destination } = result;
+
+      if (!destination) return;
+
+      if (
+        source.droppableId === destination.droppableId &&
+        source.index === destination.index
+      ) {
+        return; // Dropped at the same place
+      }
+
+      // Filter tasks by source and destination status
+      const sourceTasks = tasks.filter(
+        (task) => task.status === source.droppableId
+      );
+      const destinationTasks = tasks.filter(
+        (task) => task.status === destination.droppableId
+      );
+
+      // Find and remove the dragged task from the source list
+      const [draggedTask] = sourceTasks.splice(source.index, 1);
+
+      // If source and destination are the same, the destination tasks are the modified source tasks
+      if (source.droppableId === destination.droppableId) {
+        sourceTasks.splice(destination.index, 0, draggedTask);
+      } else {
+        // Else, update status and add to the destination list
+        draggedTask.status = destination.droppableId;
+        destinationTasks.splice(destination.index, 0, draggedTask);
+      }
+
+      // Create a new array with the updated tasks
+      const updatedTasks = tasks
+        .filter(
+          (task) =>
+            task.status !== source.droppableId &&
+            task.status !== destination.droppableId
+        )
+        .concat(sourceTasks)
+        .concat(destinationTasks);
+
+      // Update the state
+      setTasks(updatedTasks);
+    } catch (error) {
+      console.error("An error occurred during drag-and-drop", error);
+      resetServerContext();
+    }
   };
-  const upcomingTasks = tasksData.filter((task) => task.status === "plan");
-  const todayTasks = tasksData.filter((task) => task.status === "ongoing");
-  const finishedTasks = tasksData.filter((task) => task.status === "finished");
+
+  const renderTasks = (tasks, droppableId) => (
+    <Droppable droppableId={droppableId} ignoreNonContiguousIndexes>
+      {(provided) => (
+        <div
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          style={{ minHeight: "80vh" }}
+        >
+          {tasks.map((task, index) => {
+            let id = task.id;
+            return (
+              <Draggable key={id} draggableId={id} index={index}>
+                {(provided) => (
+                  <div
+                    className="Alltasks"
+                    style={{ cursor: "pointer" }}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <div className="container">
+                      <p>
+                        <span style={{ color: "#f9eaea", fontSize: "18px" }}>
+                          <strong>{task.title}</strong>
+                        </span>
+                        {task.description && (
+                          <>
+                            <br />
+                            <span style={taskDescription}>
+                              {task.description}
+                            </span>
+                          </>
+                        )}
+                        <br />
+                        <span style={{ color: "#ededed", fontSize: "14px" }}>
+                          <i className="bi bi-calendar3-event-fill"></i>
+                          <span> </span>
+                          {formatDate(task.due)}
+                        </span>
+                        <br />
+                        <span style={{ color: "silver", fontSize: "14px" }}>
+                          <i className="bi bi-tags-fill"></i> {task.label}
+                        </span>
+                        <span style={{ color: "#ededed", fontSize: "14px" }}>
+                          <i className="bi bi-chevron-double-right"></i>{" "}
+                          {task.assigned_to}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            );
+          })}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+
+  const upcomingTasks = tasks.filter((task) => task.status === "plan");
+  const todayTasks = tasks.filter((task) => task.status === "ongoing");
+  const finishedTasks = tasks.filter((task) => task.status === "finished");
 
   return (
     <>
-      <div className="container-fluid d-flex vh-100 bg-dark ">
+      <DragDropContext onDragEnd={handleOnDragEnd}>
         <div
           className="fixed-width-sidebar bg-black"
           style={{ width: "240px" }}
         ></div>
-        <div className="row align-items-stretch w-100">
-          <div
-            className="col-md-4 bg-primary d-flex boarddata"
-            style={{
-              overflowY: "auto",
-              maxHeight: "100%",
-              msOverflowStyle: "none",
-              scrollbarWidth: "none",
-            }}
-          >
-            <div
-              style={{
-                height: "60px",
-                marginTop: "10px",
-                width: "90%",
-                marginLeft: "auto",
-                marginRight: "auto",
-              }}
-            >
-              <div
-                className="text-center"
-                style={{
-                  backgroundColor: "rgb(211, 229, 239)",
-                  color: "rgb(24, 51, 71)",
-                  fontSize: "20px",
-                  fontFamily: "sans-serif",
-                  padding: "10px",
-                  borderRadius: "10%",
-                }}
-              >
+        <div className="container-fluid d-flex vh-100 bg-dark">
+          <div className="col-md-4 d-flex boarddata" style={ongoingMain}>
+            <div style={headingcss}>
+              <div className="text-center" style={ongoingSub}>
                 Ongoing Plans
               </div>
-              <div className="mt-4">
-                {renderTasks(todayTasks)}
-                <button
-                  className="btn rounded-circle"
-                  style={{
-                    fontSize: "18px",
-                    display: "block",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    backgroundColor: "black",
-                    color: "white",
-                    border: "none",
-                    marginBottom: "10px",
-                  }}
-                >
-                  <FaPlus />
-                </button>
-              </div>
+              <div className="mt-4">{renderTasks(todayTasks, "ongoing")}</div>
             </div>
           </div>
-          <div
-            className="col-md-4 bg-secondary d-flex"
-            style={{
-              overflowY: "auto",
-              maxHeight: "100%",
-              msOverflowStyle: "none",
-              scrollbarWidth: "none",
-            }}
-          >
-            <div
-              style={{
-                height: "60px",
-                marginTop: "10px",
-                width: "90%",
-                marginLeft: "auto",
-                marginRight: "auto",
-              }}
-            >
-              <div
-                className="text-center"
-                style={{
-                  fontSize: "20px",
-                  fontFamily: "sans-serif",
-                  padding: "10px",
-                  borderRadius: "10%",
-                  color: "rgb(65, 36, 84)",
-                  backgroundColor: "rgb(232, 222, 238)",
-                }}
-              >
+          <div className="col-md-4 d-flex" style={upcomingMain}>
+            <div style={headingcss}>
+              <div className="text-center" style={upcomingSub}>
                 Upcoming Plans
               </div>
-              <div className="mt-4">
-                {renderTasks(upcomingTasks)}
-                <button
-                  className="btn rounded-circle"
-                  style={{
-                    fontSize: "18px",
-                    display: "block",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    backgroundColor: "black",
-                    color: "white",
-                    border: "none",
-                    marginBottom: "10px",
-                  }}
-                >
-                  <FaPlus />
-                </button>
-              </div>
+              <div className="mt-4">{renderTasks(upcomingTasks, "plan")}</div>
             </div>
           </div>
-          <div
-            className="col-md-4 bg-success d-flex"
-            style={{
-              overflowY: "auto",
-              maxHeight: "100%",
-              msOverflowStyle: "none",
-              scrollbarWidth: "none",
-            }}
-          >
-            <div
-              style={{
-                height: "60px",
-                marginTop: "10px",
-                width: "90%",
-                marginLeft: "auto",
-                marginRight: "auto",
-              }}
-            >
-              <div
-                className="text-center"
-                style={{
-                  fontSize: "20px",
-                  fontFamily: "sans-serif",
-                  color: "rgb(28, 56, 41)",
-                  backgroundColor: "rgb(219, 237, 219)",
-                  padding: "10px",
-                  borderRadius: "10%",
-                }}
-              >
+          <div className="col-md-4 d-flex" style={finishedMain}>
+            <div style={headingcss}>
+              <div className="text-center" style={finishedSub}>
                 Finished
               </div>
-              <div className="mt-4">{renderTasks(finishedTasks)}</div>
+              <div className="mt-4">
+                {renderTasks(finishedTasks, "finished")}
+              </div>
             </div>
           </div>
+          <MyButton />
         </div>
-      </div>
+      </DragDropContext>
     </>
+  );
+}
+
+function MyButton() {
+  return (
+    <button
+      className="btn rounded-circle AddButton" // Updated class name
+      id="AddButton"
+    >
+      <FaPlus />
+    </button>
   );
 }
---
+++ src/Board/BoardStyles.js
@@ -0,0 +1,61 @@
+export const finishedMain = {
+  overflowY: "auto",
+  maxHeight: "100%",
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
+  backgroundColor: "#407340",
+};
+export const finishedSub = {
+  fontSize: "20px",
+  fontFamily: "sans-serif",
+  color: "rgb(28, 56, 41)",
+  backgroundColor: "rgb(219, 237, 219)",
+  padding: "10px",
+  borderRadius: "10%",
+};
+export const upcomingMain = {
+  overflowY: "auto",
+  maxHeight: "100%",
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
+  backgroundColor: "rgb(70, 59, 69)",
+};
+export const upcomingSub = {
+  fontSize: "20px",
+  fontFamily: "sans-serif",
+  padding: "10px",
+  borderRadius: "10%",
+  color: "rgb(65, 36, 84)",
+  backgroundColor: "rgb(232, 222, 238)",
+};
+export const ongoingMain = {
+  overflowY: "auto",
+  maxHeight: "100%",
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
+  backgroundColor: "#4b5e77",
+};
+export const ongoingSub = {
+  backgroundColor: "rgb(211, 229, 239)",
+  color: "rgb(24, 51, 71)",
+  fontSize: "20px",
+  fontFamily: "sans-serif",
+  padding: "10px",
+  borderRadius: "10%",
+};
+export const headingcss = {
+  height: "60px",
+  marginTop: "10px",
+  width: "90%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  
+};
+export const taskDescription = {
+  color: "#ededed",
+  fontSize: "12px",
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
+  margin: "none",
+  padding: "none",
+};
