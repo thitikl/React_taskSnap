bc5904b0d2f4dcb36f9b8d4cfbced1626f05a62a
Tony K <thitiklinpon@gmail.com>
2024-01-12 11:02:18 -0500

    Add drag and drop logic ***Still in progress

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import moment from "moment";
 import { useState, useEffect } from "react";
 import TaskModal from "./TaskModal";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function Board(props) {
   var data = props.data;
@@ -33,28 +34,83 @@ function Board(props) {
   var columnOrder = ["plan", "ongoing", "finished"];
 
   const renderTasks = (status) => {
-    console.log(columns[status].tasks);
-    return columns[status].tasks.map((task) => (
-      <div
-        className="board-task"
-        key={task.id}
-        onClick={() => handleClickOpenDialog(task)}
-      >
-        <h3 className="board-task-title">{task.title}</h3>
-        <p>
-          {task.dueDate != ""
-            ? task.dueTime != ""
-              ? "📅 " +
-                moment(task.dueDate).format("MMM DD, YYYY ") +
-                moment(task.dueTime, "HH:mm:ss").format("h:mm A")
-              : "📅 " + moment(task.dueDate).format("MMM DD, YYYY")
-            : ""}
-        </p>
-        <p>🏷️ {task.label}</p>
-      </div>
+    return columns[status].tasks.map((task, index) => (
+      <Draggable draggableId={task.id} index={index} key={task.id}>
+        {(provided, snapshot) => (
+          <div
+            className="board-task"
+            key={task.id}
+            onClick={() => handleClickOpenDialog(task)}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <h3 className="board-task-title">{task.title}</h3>
+            <p>
+              {task.dueDate != ""
+                ? task.dueTime != ""
+                  ? "📅 " +
+                    moment(task.dueDate).format("MMM DD, YYYY ") +
+                    moment(task.dueTime, "HH:mm:ss").format("h:mm A")
+                  : "📅 " + moment(task.dueDate).format("MMM DD, YYYY")
+                : ""}
+            </p>
+            <p>🏷️ {task.label}</p>
+          </div>
+        )}
+      </Draggable>
     ));
   };
 
+  // Drag and drop logic
+  var onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    } else if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    } else {
+      const start = columns[source.droppableId];
+      const finish = columns[destination.droppableId];
+      if (start.id === finish.id) {
+        const newTasks = Array.from(start.tasks);
+        newTasks.splice(source.index, 1);
+        var task = data.find((task) => task.id == draggableId);
+        newTasks.splice(destination.index, 0, task);
+        setColumns({
+          ...columns,
+          [start.id]: { ...start, tasks: newTasks },
+        });
+        return;
+      } else {
+        const startTasks = Array.from(start.tasks);
+        startTasks.splice(source.index, 1);
+        const newStart = {
+          ...start,
+          tasks: startTasks,
+        };
+        const finishTasks = Array.from(finish.tasks);
+        var task = data.find((task) => task.id == draggableId);
+        finishTasks.splice(destination.index, 0, task);
+        const newFinish = {
+          ...finish,
+          tasks: finishTasks,
+        };
+        const newColumns = {
+          ...columns,
+          [source.droppableId]: newStart,
+          [destination.droppableId]: newFinish,
+        };
+        setColumns(newColumns);
+        return;
+      }
+    }
+  };
+
+  // Modal logic
   const [selectedTask, setSelectedTask] = useState(null);
   const [showModal, setShow] = useState(false);
 
@@ -82,29 +138,41 @@ function Board(props) {
 
   return (
     <div className="content" id="board">
-      <div className="board-all-status">
-        {columnOrder.map((status) => {
-          return (
-            <div className="board-status" key={status.id}>
-              <h1>{columns[status].title}</h1>
-              <hr />
-              <div className="board-tasks-container">
-                {columns[status].tasks.length > 0
-                  ? renderTasks(status)
-                  : "No Task to display"}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      {showModal && (
-        <TaskModal
-          task={selectedTask}
-          showModal={showModal}
-          onHide={handleCloseModal}
-          onSave={handleCloseModalWithChange}
-        />
-      )}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="board-all-status">
+          {columnOrder.map((status) => {
+            return (
+              <Droppable droppableId={status} key={status}>
+                {(provided) => (
+                  <div
+                    className="board-status"
+                    key={status.id}
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                  >
+                    <h1>{columns[status].title}</h1>
+                    <hr />
+                    <div className="board-tasks-container">
+                      {columns[status].tasks.length > 0
+                        ? renderTasks(status)
+                        : "No Task to display"}
+                    </div>
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            );
+          })}
+        </div>
+        {showModal && (
+          <TaskModal
+            task={selectedTask}
+            showModal={showModal}
+            onHide={handleCloseModal}
+            onSave={handleCloseModalWithChange}
+          />
+        )}
+      </DragDropContext>
     </div>
   );
 }
